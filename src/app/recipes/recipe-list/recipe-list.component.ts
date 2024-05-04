4c8305f6151a5504a6eb41971ec9b2b489cd9a19
nitinrathodiya <nitin.rathodiya@gmail.com>
2024-05-05 00:43:14 +0530

    Basic Flow Setup for Recipe Book list, item , shopping list

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrl: './recipe-list.component.css'
+})
+export class RecipeListComponent implements OnInit{
+  recipes : Recipe[] = [
+    new Recipe('Biryani' ,'Combination of Rice with Chicken','https://t3.ftcdn.net/jpg/01/96/80/24/360_F_196802485_VQxk0qmyPGTq56rKYXGikVGApD3A7v5T.jpg')
+    ,new Recipe('Biryani' ,'Combination of Rice with Chicken','https://t3.ftcdn.net/jpg/01/96/80/24/360_F_196802485_VQxk0qmyPGTq56rKYXGikVGApD3A7v5T.jpg')
+  ];
+  constructor(){}
+  ngOnInit(): void {
+    
+  }
+}
